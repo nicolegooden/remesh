99592f01203d1eb2bc87e19852a3f0b86923573a
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-11 15:31:35 -0700

    Set up port and start-up feedback for server

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./app.js');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Remesh Server';
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`);
+});
