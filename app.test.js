460a1b98b20929029b3917913c553ab00d550d2c
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-11 15:32:30 -0700

    Add test for successful return of conversations

---
+++ app.test.js
@@ -0,0 +1,19 @@
+import "@babel/polyfill";
+import request from "supertest";
+import app from "./app";
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
+describe('GET /api/v1/conversations', () => {
+  it('should return a 200 and all of the conversations', async () => {
+    const expectedConversations = await database('conversations').select()
+    const res = await request(app).get('/api/v1/conversations')
+    const conversations = res.body
+
+    expect(res.status).toBe(200)
+    expect(conversations).toEqual(expectedConversations)
+  })
+})
+
