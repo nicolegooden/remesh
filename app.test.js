750b1ab678855c9676798e3bb304f26424651c9a
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-13 16:15:51 -0700

    Add sad path unit test for messages belonging to nonexistent conversation

--- app.test.js
+++ app.test.js
@@ -20,16 +20,20 @@ describe('GET /api/v1/conversations', () => {
 describe('GET /api/v1/messages/:conversation', () => {
   it('should return a 200 and messages for single conversation', async () => {
     const expectedMessages = await database('messages')
-    .where('conversation_id', '1')
-    .select();
-
+    .where('conversation_id', '1').select();
     const res = await request(app).get('/api/v1/messages/1');
     const result = res.body;
 
-    console.log('result', result)
-
     expect(res.status).toBe(200);
     expect(result).toEqual(expectedMessages);
+  })
+
+  it('should return a 404 and the message "No messages found with the conversation id 5"', async () => {
+    const response = await request(app).get('/api/v1/messages/5');
+    const { error } = response.body;
+
+    expect(response.status).toBe(404);
+    expect(error).toEqual('No messages found with the conversation id 5');
   })
 })
 
