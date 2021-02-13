50d143992a31186b49fc66908fb53e99fbd55271
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-13 15:53:47 -0700

    Add passing test for GET conversations

--- app.test.js
+++ app.test.js
@@ -8,12 +8,12 @@ const database = require('knex')(configuration)
 
 describe('GET /api/v1/conversations', () => {
   it('should return a 200 and all of the conversations', async () => {
-    const expectedConversations = await database('conversations').select()
-    const res = await request(app).get('/api/v1/conversations')
-    const conversations = res.body
+    const expectedConversations = await database('conversations').select();
+    const res = await request(app).get('/api/v1/conversations');
+    const conversations = res.body;
 
-    expect(res.status).toBe(200)
-    expect(conversations).toEqual(expectedConversations)
+    expect(res.status).toBe(200);
+    expect(conversations).toEqual(expectedConversations);
   })
 })
 
