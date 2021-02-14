3ef15849cb110f8b4c17a60c24de9fa17648e871
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-13 23:04:34 -0700

    Change message that's logged when server is running

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Remesh Server';
+app.locals.title = 'Server';
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`);
